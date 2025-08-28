93385cc25dffdc9649c5a50ae24a80999251d715
Tomasitoou <tomas.ortizp@uniagustiniana.edu.co>
2025-08-28 15:55:16 -0500

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "tareas-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js"
+];
+
+// Instalación: cachea los archivos
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activación: elimina caches antiguos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepción de peticiones
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
